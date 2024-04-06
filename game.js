1999eebdb17fa59dc31b4b3fbc0d1883e248ea0c
Aria <ariari.elk@gmail.com>
2024-04-05 20:36:39 -0500

    Initial commit

---
+++ game.js
@@ -0,0 +1,10 @@
+const gameDisk = () => ({
+  roomId: 'bar', // Set this to the ID of the room you want the player to start in.
+  rooms: [
+    {
+      id: 'bar',
+      name: 'Bar',
+      desc: `This is the sentence that will play at the start of the game.`
+    }
+      ],
+    });
